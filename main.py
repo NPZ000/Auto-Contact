ca14b1d5d00713120f7d6d2b8ef33b90884c2765
Npz <30956725+NPZ000@users.noreply.github.com>
2025-05-07 23:11:27 +0800

    feat: 支持过滤职位

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
+import easyocr
+from pathlib import Path
 
 
 class JobClicker:
@@ -18,7 +20,44 @@ class JobClicker:
             'Vue',
             '小程序'
         ]
-        self.max_count = 30 # 最多打招乎次数
+        self.max_count = 40 # 最多打招乎次数
+        self.location = '北京' # 区域
+        self.salary_num = 22 # 薪资区间的最高点，没有达到这个数字就会被过滤掉
+        # 要排除的公司
+        self.exclude_company_list = [
+            '汉克',
+            '七凌',
+            '法本',
+            '纬创',
+            '柯莱特',
+            '德科',
+            '数字马力',
+            '金道天成',
+            '慧博云通',
+            '亿达',
+            '软通',
+            '科锐尔',
+            '字节',
+            '慧教研',
+            '神州通誉',
+            '国简科技',
+            '浪潮数字',
+            '六易在线',
+            '微创'
+        ]
+    
+    # 删除截出来的薪资图片
+    def del_img(self, path):
+        file_path = Path(path)
+        try:
+            file_path.unlink()
+            print(f"文件 {file_path} 删除成功！")
+        except FileNotFoundError:
+            print(f"文件 {file_path} 不存在！")
+        except PermissionError:
+            print(f"没有权限删除 {file_path}！")
+        except Exception as e:
+            print(f"删除失败：{e}")
 
     def set_cookie(self):
         cookie = {
@@ -42,10 +81,31 @@ class JobClicker:
             # 过滤掉不相关的职位
             filter_list = []
             for element in box_elements:
+                # 职位名称
                 job_name = element.find_element(By.CLASS_NAME, "job-name").text
-                print(f'职位名称={job_name}')
+                # 薪资元素
+                salary = element.find_element(By.CLASS_NAME, "job-salary")
+                salary.screenshot(f'./{job_name}.png')
+                # 创建reader对象
+                reader = easyocr.Reader(['ch_sim','en']) 
+                # 读取图像中的薪资数字
+                result = reader.readtext(f'./{job_name}.png')
+                salary_str = result[0][1]
+                # 取出来薪资范围中的最大值
+                max_salary = salary_str.split('K')[0].split('-')[1]
+                print(f'salary-->{result[0][1]}---{max_salary}')
+                self.del_img(f'./{job_name}.png')
+                # 公司名称
+                boss_name = element.find_element(By.CLASS_NAME, "boss-name").text
+                # 地址
+                location = element.find_element(By.CLASS_NAME, "company-location").text
+                # 按职位名称 公司名称 地址 薪资过滤
                 if any(keyword in job_name for keyword in self.used_job_name_list):
-                    filter_list.append(element)
+                    if all(company_name not in boss_name for company_name in self.exclude_company_list):
+                        if self.location in location and int(max_salary) >= self.salary_num:
+                            print(f'职位名称={job_name}-公司名称={boss_name}-地址={location}--薪资={max_salary}')
+                            filter_list.append(element)
+
 
             for box in filter_list:
                 try:
@@ -77,17 +137,19 @@ class JobClicker:
             self.set_cookie()
             self.driver.refresh()
 
+            
+
             while self.click_count < self.max_count:
                 # 刷新页面
-                self.driver.refresh()
-                # 找到职位按钮点击
+                # self.driver.refresh()
+                # # 找到职位按钮点击
+                # WebDriverWait(self.driver, 5).until(
+                #     EC.visibility_of_element_located((By.LINK_TEXT, "职位"))
+                # ).click()
+                self.driver.get("https://www.zhipin.com/web/geek/jobs?salary=405")
+                time.sleep(3)
                 WebDriverWait(self.driver, 5).until(
-                    EC.visibility_of_element_located((By.LINK_TEXT, "职位"))
-                ).click()
-                time.sleep(5)
-                # 如果有特定的求职期望，替换下面的“职位（城市）”
-                WebDriverWait(self.driver, 5).until(
-                    EC.visibility_of_element_located((By.LINK_TEXT, "前端开发工程师(北京)"))
+                    EC.visibility_of_element_located((By.LINK_TEXT, f"前端开发工程师({self.location})"))
                 ).click()
                 time.sleep(3)
                 self.click_job()
@@ -96,7 +158,7 @@ class JobClicker:
         except Exception as exc:
             print('error', exc)
         finally:
-            time.sleep(10)
+            time.sleep(100)
             self.driver.quit()
 
 
